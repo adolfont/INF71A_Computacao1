32d6c2514d2e3e8c3fc43e136d5e6ccea5ab179f
Adolfo Neto <adolfo.usp@gmail.com>
2024-10-15 20:06:56 -0300

    if

---
+++ 3_if/1_if.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    float peso = 100.5;
+
+    if (peso > 100)
+    {
+        printf("O peso (%f) é superior a 100!!!", peso);
+    }
+    else
+    {
+        printf("O peso (%f) não é superior a 100!!!", peso);
+    }
+
+    // fazer com scanf
+    return 0;
+
+}
