1bd88db2012c63e0d1ec804e6b6237550e476afd
Adolfo Neto <adolfo.usp@gmail.com>
2024-10-22 11:32:40 -0300

    mais um

--- 2_2_Entrada_Saida/3_scanf_char.c
+++ 2_2_Entrada_Saida/3_scanf_char.c
@@ -2,10 +2,14 @@
 int main()
 {
     char nome[100];
+    char sobrenome[100];
 
     printf("Digite seu nome: ");
     scanf("%99s", nome);
 
-    printf("O nome que você digitou foi '%s'\n", nome);
+    printf("Digite seu sobrenome: ");
+    scanf("%99s", sobrenome);
+
+    printf("O nome que você digitou foi '%s %s'\n", nome, sobrenome);
     return 0;
 }
