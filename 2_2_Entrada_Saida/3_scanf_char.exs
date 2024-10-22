9f65ee64b2f7ecf11dc3c92393701590332a832b
Adolfo Neto <adolfo.usp@gmail.com>
2024-10-22 11:42:12 -0300

    Exemplo em Elixir

---
+++ 2_2_Entrada_Saida/3_scanf_char.exs
@@ -0,0 +1,4 @@
+nome = IO.gets("Digite seu nome: ") |> String.trim()
+sobrenome = IO.gets("Digite seu sobrenome: ") |> String.trim()
+
+IO.puts("O nome que você digitou foi '#{nome} #{sobrenome}'\n")
