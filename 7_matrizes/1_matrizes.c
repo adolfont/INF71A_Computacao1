3a692013bd6c74336be90a69e3cdf4086b8462c6
Adolfo Neto <adolfo.usp@gmail.com>
2024-12-04 10:34:21 -0300

    Matrizes

---
+++ 7_matrizes/1_matrizes.c
@@ -0,0 +1,13 @@
+int main(){
+
+    int escaninho[5][10];
+
+    escaninho[2][3] = 10;
+
+    printf("escaninho[%d,%d] = %d\n",2, 3, escaninho[2][3] );
+
+    escaninho[4][6] = 100;
+
+    printf("escaninho[%d,%d] = %d\n",4, 6, escaninho[4][6] );
+    printf("escaninho[%d,%d] = %d\n",3, 3, escaninho[3][3] );
+}
