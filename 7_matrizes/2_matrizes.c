15f447354d1d5e5fbc9c76ad51ad43135f57426a
Adolfo Neto <adolfo.usp@gmail.com>
2024-12-04 10:39:11 -0300

    Exercicio

---
+++ 7_matrizes/2_matrizes.c
@@ -0,0 +1,19 @@
+int main(){
+
+    int escaninho[5][10];
+
+    escaninho[2][3] = 10;
+
+    printf("escaninho[%d,%d] = %d\n",2, 3, escaninho[2][3] );
+
+    escaninho[4][6] = 100;
+
+    printf("escaninho[%d,%d] = %d\n",4, 6, escaninho[4][6] );
+    printf("escaninho[%d,%d] = %d\n",3, 3, escaninho[3][3] );
+
+    COMPLEMENTE ESTE PROGRAMA PARA QUE ELE LEIA DO TECLADO TODOS OS VALORES DA MATRIZ
+
+    DEPOIS, MULTIPLIQUE TODOS OS VALORES DA MATRIZ POR 2
+
+    EXIBA TODA A MATRIZ
+}
