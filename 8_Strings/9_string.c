a7991fd43906d05e7808ae63a1c9583fd24835fb
Adolfo Neto <adolfo.usp@gmail.com>
2024-12-18 09:54:38 -0300

    Exercício 1 de 18/12/2024

---
+++ 8_Strings/9_string.c
@@ -0,0 +1,9 @@
+/*
+Faça um programa que receba uma frase e conte as vogais, apresentando uma saída tal como a ilustrada abaixo.
+Exemplo: para a frase Na proxima quarta-feira eh feriado.
+a : ****** (6)
+e : *** (3)
+i : *** (3)
+o : ** (2)
+u : * (1)
+*/
