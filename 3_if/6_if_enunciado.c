43aea936beb4d64fc8d7eac407d55c56a0f2d9a4
Adolfo Neto <adolfo.usp@gmail.com>
2025-04-02 10:57:31 -0300

    Update 6_if_enunciado.c

--- 3_if/6_if_enunciado.c
+++ 3_if/6_if_enunciado.c
@@ -1,15 +1,18 @@
 **Exercício:**  
 
-Nome do exercício: 5_if
+Nome do exercício: 6_if
 
-_O nome do arquivo deve ser notas.c._
+_O nome do arquivo deve ser notas_2.c._
 
-Escreva um programa em C que leia três notas de um aluno e calcule a média aritmética delas. Com base na média, o programa deve exibir a seguinte classificação:  
+Escreva um programa em C que leia três notas de um aluno e calcule a média aritmética delas. 
+Leia também a frequência, um valor inteiro entre 0 e 100.
+  
+Com base na média e na frequência, o programa deve exibir a seguinte classificação:  
 
-- **Média maior que 8** → "Aprovado com Distinção"  
-- **Média maior ou igual a 6** → "Aprovado Direto"  
+- **Média maior que 8** e Frequência maior ou igual a 75% → "Aprovado com Distinção"  
+- **Média maior ou igual a 6** ou → "Aprovado Direto"  
 - **Média maior ou igual a 4** → "Vai para Final"  
-- **Média menor que 4** → "Reprovado Direto"  
+- **Média menor que 4** ou Frequência menor ou igual a 50% → "Reprovado Direto"  
 
 O programa deve solicitar ao usuário que insira três notas (valores reais) e, após calcular a média, exibir a mensagem correspondente à situação do aluno.  
 
