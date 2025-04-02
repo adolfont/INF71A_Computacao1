81d43d9023cdf5f08e3d73b9bfc71851c440e2e3
Adolfo Neto <adolfo.usp@gmail.com>
2025-04-02 09:44:41 -0300

    Add files via upload

--- 3_if/4_if.c
+++ 3_if/4_if.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<locale.h>
 int main()
 {
+    setlocale(LC_ALL, "Portuguese");
     int numero1, numero2;
 
     printf("Digite dois números inteiros, separados por espaços: ");
