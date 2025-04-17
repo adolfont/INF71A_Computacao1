85e3e5ce375788c9fd065ef9c45bada13011d5d9
Adolfo Neto <adolfo.usp@gmail.com>
2025-04-17 15:18:13 -0300

    Create for_exercicio.c

---
+++ 4_repeticao/for_exercicio.c
@@ -0,0 +1,19 @@
+**Exercício:**  
+
+Nome do exercício: for_1
+
+_O nome do arquivo deve ser for_1.c_
+
+Inclua o link para a resposta em <https://forms.gle/JdD97kvGm28LPJZT6>.
+
+Escreva um programa em C que leia um valor inteiro n. 
+
+Apresente todos os números de 1 a 200 que divididos por n são pares e dão resto igual a 3.
+
+Use a estrutura de repetição 'for'.
+
+## Exemplo
+```
+Digite um valor inteiro n: 15
+18 48 78 108 138 168 198
+```
