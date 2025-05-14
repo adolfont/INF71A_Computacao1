c4b7514bdeaeeb9dbf081ae3cafb70e987cb271f
Adolfo Neto <adolfo.usp@gmail.com>
2025-05-14 10:12:12 -0300

    Create 6b_vetores.c

---
+++ 6_vetores/6b_vetores.c
@@ -0,0 +1,17 @@
+
+/*
+
+Implemente um programa em C que preenche os elementos de um vetor de forma aleatória.
+
+Use uma constante TAMANHO para definir o tamanho do vetor.
+
+Escreva todos os valores do vetor.
+
+Depois disso, peça para o usuário digitar um valor.
+
+Se o valor não estiver no vetor, insira-o na última posição do vetor.
+
+Se o valor estiver no vetor,  o programa deve alertar a duplicidade e solicitar
+um novo valor.
+
+*/
