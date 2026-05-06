ade47bd15ade94c89279c225c3d62683e1ba2747
Adolfo Neto <adolfo.usp@gmail.com>
2026-05-06 09:26:07 -0300

    Add files via upload

---
+++ C/4_repeticao/adivinhacao_while.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<locale.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "Portuguese");
+    srand(time(0));
+
+
+    int numero_secreto = (rand()%10)+1;
+    int chute=numero_secreto+1;
+
+    while(chute != numero_secreto)
+    {
+//        printf("O número secreto é %d \n", numero_secreto);
+        printf("Digite o número secreto: ");
+        scanf("%d", &chute);
+
+        if (chute>numero_secreto)
+            printf("O número que você chutou é maior que o número secreto!\n");
+        else if (chute<numero_secreto)
+            printf("O número que você chutou é menor que o número secreto!\n");
+
+    }
+    printf("Parabéns, você acertou!");
+}
